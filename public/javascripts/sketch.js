f57402881db170904b8a6a982e985625b68b1202
Sawrose Tamang <coco.tngg@gmail.com>
2018-06-26 01:20:34 +0545

    Bubble Manage

--- public/javascripts/sketch.js
+++ public/javascripts/sketch.js
@@ -1,28 +1,28 @@
 let bg;
 let clear = false;
 let bubbles = [];
-let canvaswidth = 900;
-let canvasheight = 550;
+let canvaswidth = 805;
+let canvasheight = 500;
 let pressTime, releaseTime;
 
 function setup() {
-  	bg = loadImage("../images/image1.jpg");
+  	bg = loadImage("../images/image2.jpg");
   	createCanvas(canvaswidth, canvasheight);
 
-  	if(clear == true) {
+  	//if(clear == true) {
 	  	setInterval(function() {
 	    	setTimeout (function(){ 
 				bubbles.shift(); 
 			}, 5000);
 	    }, 10000);
-	}
+	//}
 }
 
 function mouseReleased(){
 	if(mouseX <= canvaswidth && mouseY <= canvasheight) {
 		releaseTime = new Date();
 		
-		if(pressTime.getSeconds()+2 <= releaseTime.getSeconds()) {
+		if(pressTime.getSeconds()+1 <= releaseTime.getSeconds()) {
 			//For Multiple Bubbles Display
 			/*for(var i=0; i<7; i++) {
 			    let r = random(10, 50);
@@ -30,7 +30,7 @@ function mouseReleased(){
 				bubbles.push(b);
 			}*/
 			clear = true;
-			let r = random(10, 50);
+			let r = random(200, 300);
 			let b = new Bubble(mouseX, mouseY, r);
 			bubbles.push(b);
 	  	}
@@ -44,14 +44,13 @@ function mousePressed() {
 
 function draw() {
 	background(bg);
-
   	for (let bubble of bubbles) {
-  		bubble.move(random(0, 1));
+  		//bubble.move(random(0, 1));
     	bubble.show();
   	}
 
   	for (let i = 0; i < bubbles.length; i++) {
-    	bubbles[i].move(random(0, 1));
+    	//bubbles[i].move(random(0, 1));
     	bubbles[i].show();
   	}
 }
@@ -70,8 +69,8 @@ class Bubble {
 
   	show() {
     	stroke(233);
-    	strokeWeight(1);
-    	fill(214,206,206);
+    	strokeWeight(0.01);
+    	fill('rgba(0,191,165,0.1)');
     	ellipse(this.x, this.y, this.r * 2);
   	}
 }
